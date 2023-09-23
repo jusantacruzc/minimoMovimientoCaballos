4c35c15dad3fec77701d71eaa9d9214f3d8264a0
jusantacruzc <111158167+jusantacruzc@users.noreply.github.com>
2023-09-22 20:07:32 -0500

    Update borrar.js

--- borrar.js
+++ borrar.js
@@ -4,7 +4,7 @@ class Grafo {
         this.matrizAdyacencia = Array.from({ length: vertices }, () => Array(vertices).fill(0));
     }
 
-    generarCostos(entrada) {
+    generarCostos(entrada, salida) {
         // Movimientos posibles para cada casilla
         const movimientosCaballo = [
             [2, 1], [1, 2], [-1, 2], [-2, 1],
@@ -13,7 +13,7 @@ class Grafo {
 
         // Verificar que sea un movimiento valido
         function esMovimientoValido(x, y) {
-            return x >= 0 && x < 8 && y >= 0 && y < 8;
+            return x >= 0 && x < 8 && y >= 0 && y < 8 && (x != entrada[0] || y != entrada[1]);
         }
 
         
@@ -22,15 +22,19 @@ class Grafo {
 
         // Realmente cola solo tiene un objeto que se va agregando y eliminando hasta que ya no hayan movimientos validos, en ese punto se detiene el ciclo
         while (cola.length > 0) {
-            console.log(cola);
+
             const { x, y, costo } = cola.shift();
+            if(salida[0] == x && salida[1] == y){
+                return costo;
+            }
+            
             this.matrizAdyacencia[x][y] = costo;
 
             for (const [dx, dy] of movimientosCaballo) {
-                const nuevoX = x + dx;
-                const nuevoY = y + dy;
-                if (esMovimientoValido(nuevoX, nuevoY) && this.matrizAdyacencia[nuevoX][nuevoY] === 0 && (nuevoX != entrada[0] || nuevoY != entrada[1])) {
-                    cola.push({ x: nuevoX, y: nuevoY, costo: costo + 1 });
+                const Nx = x + dx;
+                const Ny = y + dy;
+                if (esMovimientoValido(Nx, Ny) && this.matrizAdyacencia[Nx][Ny] === 0 ) {
+                    cola.push({ x: Nx, y: Ny, costo: costo + 1 });
                 }
             }
         }
@@ -39,14 +43,16 @@ class Grafo {
 
 const grafo = new Grafo(8);
 // Coordenadas del tablero
-const entrada = [0, 0]; 
-const salida = [3,3];
+const entrada = [0,0]; 
+const salida = [7,7];
 
-// Costos en los mosvimientos del caballo - la forma mas rapida de llegar a cada casilla
-grafo.generarCostos(entrada);
-console.log(grafo.matrizAdyacencia[salida[0]][salida[1]]);
+// Costos en los movimientos del caballo - la forma mas rapida de llegar a cada casilla
+console.log(grafo.generarCostos(entrada, salida));
 
-// // imprimir Matriz de adyacnecia
+// console.log(grafo.matrizAdyacencia[salida[0]][salida[1]]);
+
+
+// imprimir Matriz de adyacnecia
 // function imprimitGrafo(grafo) {
 //     for (let i = 0; i < grafo.vertices; i++) {
 //         let row = "";
@@ -56,5 +62,6 @@ console.log(grafo.matrizAdyacencia[salida[0]][salida[1]]);
 //         console.log(row);
 //     }
 // }
+
 
 // imprimitGrafo(grafo);
